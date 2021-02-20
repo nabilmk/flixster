a4d9966be237be1d8ab1134fdec21e142e3f190b
Nabil Khan <nb768q@gmail.com>
2021-02-19 21:13:49 -0500

    added superhero tab with grid view

---
+++ flamingo/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flamingo
+//
+//  Created by Nabil Khan on 2/19/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell{
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
